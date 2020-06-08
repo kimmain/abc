9b1f0980c1ec8de39b3d5cc0cd3e0a2035e5227f
KGITBank <KGITBank@508-023>
2020-06-08 14:58:38 +0900

    ㅎㅅㅎ

---
+++ 200608/src/main/java/com/care/instal/HomeController.java
@@ -0,0 +1,44 @@
+package com.care.instal;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";	//.jsp생략
+
+	}
+	@RequestMapping("test")	//http://localhost:8000/test
+		public String test01() {
+		return "aaa";	//test랑 이름 안맞아도 상관없음 대신 .jsp페이지 보여주는거니까 그거있어야함
+	}
+	
+}
